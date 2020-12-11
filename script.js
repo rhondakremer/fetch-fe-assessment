01968fd14afbf87e89842b6d22d145a064885801
Rhonda Kremer <rhondakremer@gmail.com>
2020-12-10 20:08:58 -0500

    initial sanitizing and sorting idea

--- script.js
+++ script.js
@@ -5,8 +5,27 @@ function fetchData() {
     fetch(corsAnywhereProxy + fetchApi)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            sanitizeAndSortData(data);
         })
+}
+
+function sanitizeAndSortData(data) {
+    let listIdObj = {};
+    for (let i = 0; i < data.length; i++) {
+        let current = data[i];
+        if (current.name) {
+            if (current.listId in listIdObj) {
+                listIdObj[current.listId].push(current);
+            }
+            else {
+                listIdObj[current.listId] = [current];
+            }
+        }
+    }
+    for (const listId in listIdObj) {
+        listIdObj[listId].sort((a,b) => a.name.split(" ")[1] - b.name.split(" ")[1]);
+    }
+    console.log(listIdObj)
 }
 
 fetchData();
